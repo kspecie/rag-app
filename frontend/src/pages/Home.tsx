db028f30a5dc2aecb2596f06726528fb926f9a0c
Kayla Specie <krspecie@gmail.com>
2025-07-13 21:45:59 +0100

    setup frontend folder. using react + vite. installed shadcn for components

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+function Text() {
+    return(
+    <p>This is my test</p>
+    );
+};
+
+export default function Test(){
+    return (
+        <div>
+            <Text />
+            <Text />
+            <Text />
+        </div>
+    )
+}
